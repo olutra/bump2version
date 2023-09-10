87f530f6f7429019fa4e35671e82146ce1d1f033
olutra <>
2023-09-10 17:13:38 +0300

    Add pypy3.10 to the test docker image

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11", "pypy3.7", "pypy3.8", "pypy3.9"])
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11", "pypy3.7", "pypy3.8", "pypy3.9", "pypy3.10"])
 def tests(session):
     session.install("pytest", "testfixtures", "pytest-xdist")
     session.run("pip", "install", ".")
